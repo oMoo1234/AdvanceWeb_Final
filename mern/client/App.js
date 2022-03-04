00ea9f8e715081a8a43ac36dd6b165c8532a9c82
O Moo <omoog10101@gmai.com>
2022-03-04 01:42:44 -0700

    deleted uneeded floders

---
+++ mern/client/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+
+// We import all the components we need in our app
+import Navbar from "./src/components/navbar";
+import RecordList from "./src/components/recordList";
+import Edit from "./src/components/edit";
+import Create from "./src/components/create";
+
+const App = () => {
+ return (
+   <div>
+     <Navbar />
+     <Routes>
+       <Route exact path="/" element={<RecordList />} />
+       <Route path="/edit/:id" element={<Edit />} />
+       <Route path="/create" element={<Create />} />
+     </Routes>
+   </div>
+ );
+};
+
+export default App;
